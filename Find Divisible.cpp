9dd2922a5250ae6d8aeb2b55893a12773df23af5
Mayank Mishra <53926096+soulprogrammer01@users.noreply.github.com>
2020-08-24 21:30:00 +0530

    Add files via upload

---
+++ Find Divisible.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	 long long int t,l,r,x,y;
+	 cin>>t;
+	 while(t--)
+	 {  
+	 	cin>>l>>r;
+	    x=l;
+	    y=(l+l);
+	    cout<<x<<y<<endl;
+	 }
+
+return 0;
+}
---
+++ omkar and completion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main() {
+	 long long int t,n;
+	 int arr[100000];
+	 cin>>t;
+	 while(t--)
+	 {  
+	 	cin>>n;
+	 	for(int i=0;i<n;i++)
+	 	{
+        	cout<<"1"<<" ";
+	 	}
+     cout<<endl;
+ 
+	 }
+ 
+	return 0;
+}
